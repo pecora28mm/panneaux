efc56af1ae9a2b195f46421f7becd2cf909238b2
Perrick Penet-Avez <perrick@pecora28mm.org>
2014-04-18 14:27:32 +0200

    Ajout de la page pour le A Propos

---
+++ panneaux/pages/apropos.php
@@ -0,0 +1,22 @@
+<h2>Le contexte</h2>
+<p>suivre précisement l'avancement l'affichage de nos affiches sur les panneaux électoraux de France et de Navarre.</p>
+
+<h2>L'usage</h2>
+
+<h3>Pour un militant</h3>
+<ol>
+	<li>je prépare ma tournée en indiquant les 2, 20 ou 200 panneaux sur lesquels je vais coller une affiche. L'application m'imprime une liste papier avec toutes les adresses et l'état du panneau (vide, recouvert, nickel).</li>
+	<li>à la fin de ma tournée, je reviens chez moi et je coche tous les panneaux que j'ai collé sur l'appliation web.</li>
+</ol>
+
+<h3>Pour un sympathisant</h3>
+<ol>
+	<li>je passe dans une rue et je vois qu'il manque un panneau.</li>
+	<li>je prends une photo avec mon smartphone. L'application détecte (plus ou moins) la localisation, me suggère une adresse précise et me propose d'indiquer l'état du moment : "panneau vide", "panneau vandalisé", "panneau recouvert", "panneau nickel".</li>
+</ol>
+
+<h3>Pour un stratège</h3>
+<ol>
+	<li>je consulte les zones où les panneaux sont vides et je peux cibler les adhérents dans les environs</li>
+	<li>j'ai un tableau de bord par région (ou sur la France entière) avec le nombre de panneax visités, collés, pré-emptés, etc...</li>
+</ol>
